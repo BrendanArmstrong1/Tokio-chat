acecf956d55ee41819f538194e365ff39f2f1596
Brendan Armstrong <Brendan.Armstrong@mail.com>
2022-03-26 19:42:22 -0400

    multiple clients with non self-returning messages

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,44 @@
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
+    sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (mut socket, _addr) = listener.accept().await.unwrap();
+    let (tx, _rx) = broadcast::channel(10);
 
-    let mut buffer = [0u8; 1024];
+    loop {
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
-    // Read message from socket connection
-    let bytes_read = socket.read(&mut buffer).await.unwrap();
+        tokio::spawn(async move {
+            let (read, mut write) = socket.split();
+            let mut reader = BufReader::new(read);
+            let mut line = String::new();
 
-    // Write the message back into the same socket
-    socket.write_all(&buffer[..bytes_read]).await.unwrap();
+            loop {
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+
+                        if addr != other_addr {
+                            write.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
+                }
+
+            }
+        });
+    }
 }
